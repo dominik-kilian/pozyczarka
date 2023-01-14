7a86970bbc81e17757e18c1df66b672a8cd064b6
dominik <shudarrr@gmail.com>
2023-01-14 21:24:01 +0100

    Create new column, create migration and add view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -66,6 +66,6 @@ class ItemsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def item_params
-    params.require(:item).permit(:name, :active, :borrowed)
+    params.require(:item).permit(:name, :active, :borrowed, :item_type)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,10 +7,13 @@
 #  id         :bigint           not null, primary key
 #  active     :boolean
 #  borrowed   :boolean
+#  item_type  :integer          default("other"), not null
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class Item < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 2}
+  validates :name, presence: true, length: { minimum: 3}
+
+  enum item_type: %i[fantasy sci-fi criminal horror other ]
 end
